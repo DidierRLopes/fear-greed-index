f8c232c54b6c7cc33fbc4154c1d2b8bb3c88bcef
James Morris <jamesmorris2@gmail.com>
2022-02-28 18:08:33 -0500

    Make imports more ergonomic
    By importing names into __init__.py and then listing them in
    __all__, we can make it so users don't have to type so much

---
+++ fear_greed_index/__init__.py
@@ -0,0 +1,7 @@
+from .CNNFearAndGreedIndex import CNNFearAndGreedIndex
+from .FearAndGreedIndicator import FearAndGreedIndicator
+
+__all__ = [
+    "CNNFearAndGreedIndex",
+    "FearAndGreedIndicator",
+]
